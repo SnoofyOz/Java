fc8ebc532316e65e7f0c5d7b554da953673d880e
Dang Nhat <chibix.hyperx@gmail.com>
2024-04-07 11:19:46 +0700

    Nhap

---
+++ Exam/src/Exam.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exam {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nhapTen = sc.nextLine();
+        String nhapTuoi = sc.nextLine();
+        System.out.println(nhapTen);
+        System.out.println(nhapTuoi);
+    }
+}
